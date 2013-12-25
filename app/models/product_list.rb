0e9ad7f869eb3acda02bf305c46cdd893078b58c
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-12-25 17:09:53 +0530

    condition removed

--- app/models/product_list.rb
+++ app/models/product_list.rb
@@ -1,11 +1,7 @@
 class ProductList < ActiveRecord::Base
 
  def self.json_tokens(query)
-    list = where("name like ?", "%#{query}%")
-    if list.empty?
-      list
-    else
-      list
-    end
-  end
+	list = where("name like ?", "%#{query}%")
+ end
+
 end
