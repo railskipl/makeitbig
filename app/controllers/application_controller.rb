d1f125f852fe4121e99abc55898c176239619406
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-12-26 16:01:27 +0530

    content change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
       @meta_keywords = seo.meta_description
       @meta_description = seo.meta_keyword
     else
-      @meta_title = "Welcome to Gifttaking.com"
-      @meta_keywords = "The gift taking web application is a comprehensive online gifting platform geared towards the gift givers."
-      @meta_description = "The gift taking web application is a comprehensive online gifting platform geared towards the gift givers."
+      @meta_title = "Welcome to FastShoppy.com"
+      @meta_keywords = "FastShoppy"
+      @meta_description = "FastShoppy"
     end
     
   end
