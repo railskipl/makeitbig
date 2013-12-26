5dc9773c04fb1e981935f5409f39a170fe9a7ef7
kipl <kipl@kipl-Inspiron-620s.(none)>
2013-12-26 16:02:21 +0530

    content change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def meta_defaults
-    seo = Seo.first
-    if seo.present?
-      @meta_title = seo.meta_title
-      @meta_keywords = seo.meta_description
-      @meta_description = seo.meta_keyword
-    else
-      @meta_title = "Welcome to FastShoppy.com"
+     @meta_title = "Welcome to FastShoppy.com"
       @meta_keywords = "FastShoppy"
-      @meta_description = "FastShoppy"
-    end
-    
+     @meta_description = "FastShoppy"
   end
 
 end
